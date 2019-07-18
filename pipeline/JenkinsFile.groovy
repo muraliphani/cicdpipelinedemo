79d0c67df682d0da6e98e98a135aceae9f92851f
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2019-07-18 20:53:49 +0530

    added

--- pipeline/JenkinsFile.groovy
+++ pipeline/JenkinsFile.groovy
@@ -9,19 +9,21 @@ node()
 		}
 		
 		
-		withEnv([
-
-                'MAVEN_HOME=' + mavenHome,
-
-                "PATH=${mavenHome}/bin:${env.PATH}"
-
-		]) 
 		stage("Build&UT")
 		{
 		mavenHome = tool(name: 'maven 3.5', type: 'maven');
 		sh"cd /devtest1"
 		sh "${mavenHome}/bin/mvn install"
 		}
-	}	
+		
+		withEnv([
+
+                'MAVEN_HOME=' + mavenHome,
+
+                "PATH=${mavenHome}/bin:${env.PATH}"
+
+		])
+		
+}	
  
 
