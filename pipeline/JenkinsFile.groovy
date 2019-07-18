5ab6bb6048031461240d3b29f68a751ff10f3d39
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2019-07-18 19:10:08 +0530

    added

--- pipeline/JenkinsFile.groovy
+++ pipeline/JenkinsFile.groovy
@@ -2,14 +2,14 @@
 
 node()
 	{
-		def mavenHome
+		def mvnHome
 		
 		stage("Build&UT")
 		{
 		checkout scm
-		mavenHome = tool(name: 'maven 3.5', type: 'maven');
+		mvnHome = tool(name: 'maven 3.5', type: 'maven');
 		sh"cd /devtest1"
-		sh "${mavenHome}/bin/mvn install"
+		sh "${mvnHome}/bin/mvn install"
 		}
 	}	
  
