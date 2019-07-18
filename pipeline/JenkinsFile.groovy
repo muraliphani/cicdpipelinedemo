a5610d294e123db28eb828b9d2fc5fe0970e02ed
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2019-07-18 21:18:41 +0530

    fdsafsa

--- pipeline/JenkinsFile.groovy
+++ pipeline/JenkinsFile.groovy
@@ -4,16 +4,22 @@ node()
 	{
 		def mavenHome
 		
-			withEnv([
+			
+		
+		stage("Checkout SCM"){
+			checkout scm
+			
+		}
+		
+		withEnv([
 
                 'MAVEN_HOME=' + mavenHome,
 
                 "PATH=${mavenHome}/bin:${env.PATH}"
 
 		])
-		
-		stage("Checkout SCM"){
-			checkout scm
+
+		stage("Build & UT"){
 		sh"cd /devtest1"
 		sh "${mavenHome}/bin/mvn install"
 		}
