b557edc231582569ec844f9377e31f944959f4bc
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2019-07-20 11:40:11 +0530

    fdsafs

--- pipeline/JenkinsFile.groovy
+++ pipeline/JenkinsFile.groovy
@@ -5,17 +5,16 @@ node()
 		def mavenHome
 		stage("Checkout SCM"){
 			checkout scm
-			sh "cd devtest1"
+			
 		}
 		stage("Build & UT"){
 		def mvnHome = tool name: 'MAVEN_HOME', type: 'maven'
+		def directory = "devtest1"
+		dir(directory){
 		def presetdir = pwd()
 		echo "$presetdir"
-		sh "cd ${presetdir}/devtest1"
-		def dire = pwd()
-		echo "$dire"
 		sh "${mvnHome}/bin/mvn clean"
-		
+		}
 		}
 	}	
  
