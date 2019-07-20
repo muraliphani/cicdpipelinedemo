242393718b4eb757cc60cc76df9d65354e128b92
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2019-07-20 10:49:09 +0530

    fdaFD

--- pipeline/JenkinsFile.groovy
+++ pipeline/JenkinsFile.groovy
@@ -6,16 +6,13 @@ node()
 		stage("Checkout SCM"){
 			checkout scm
 		}
-		
-		withEnv([ "PATH+MAVEN=${tool 'maven-3.5.2'}/bin:${env.JAVA_HOME}/bin"
-				])
-				{
 		stage("Build & UT"){
+		def mvnHome = tool name: 'MAVEN_HOME', type: 'maven'
 		sh"cd devtest1"
-		sh "mvn --batch-mode -V -U -e clean deploy -Dsurefire.useFile=false"
+		sh "${mvnHome}/bin/mvn install"
 
 		}
-		}
+		
 		
 	}	
  
