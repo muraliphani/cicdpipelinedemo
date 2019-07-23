bd109a83421b8778ead2231188cc386ec8b9fc19
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2019-07-23 18:05:24 +0530

    fdsafs

--- pipeline/JenkinsFile.groovy
+++ pipeline/JenkinsFile.groovy
@@ -3,17 +3,37 @@
 node()
 	{
 		def mavenHome
+		
 		stage("Checkout SCM"){
 			checkout scm
 			
-		}
+				}
+		
 		stage("Build & UT"){
 		def mvnHome = tool name: 'MAVEN_HOME', type: 'maven'
 		def directory = "devtest1"
 		dir(directory){
 		sh "${mvnHome}/bin/mvn install"
-		}
-		}
+					}
+			}
+			
+		stage("build & SonarQube analysis") {
+          
+              withSonarQubeEnv('My SonarQube Server') {
+                 sh 'mvn clean package sonar:sonar'
+              }
+          
+      }
+
+      stage("Quality Gate"){
+          timeout(time: 1, unit: 'HOURS') {
+              def qg = waitForQualityGate()
+              if (qg.status != 'OK') {
+                  error "Pipeline aborted due to quality gate failure: ${qg.status}"
+              }
+          }
+      }
+			
 	}	
  
 
