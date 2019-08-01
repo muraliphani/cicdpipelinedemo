396b6202e41f74849548cec7502de49caf1fbec6
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2019-08-01 17:08:03 +0530

    fdsafs

--- pipeline/JenkinsFile.groovy
+++ pipeline/JenkinsFile.groovy
@@ -32,7 +32,7 @@ node()
 	  stage('Check Quality Gate') {
         
                 echo 'Checking quality gate...'
-                timeout(time: 1, unit: 'HOURS') {
+                timeout(time: 1, unit: 'MINUITS') {
                         def swait = waitForQualityGate()
                         if (swait.status != 'OK') {
                             error "Pipeline aborted due to quality gate failure: ${swait.status}"
